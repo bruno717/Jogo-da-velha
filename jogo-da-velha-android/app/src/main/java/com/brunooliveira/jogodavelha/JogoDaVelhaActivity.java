ca9b823b228a86f559a49ee64bb5ae4a4a72ddb0
bruno717 <brunooliveira_717@hotmail.com>
2015-11-28 15:03:26 -0200

    criação do layout

---
+++ jogo-da-velha-android/app/src/main/java/com/brunooliveira/jogodavelha/JogoDaVelhaActivity.java
@@ -0,0 +1,13 @@
+package com.brunooliveira.jogodavelha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JogoDaVelhaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_da_velha);
+    }
+}
